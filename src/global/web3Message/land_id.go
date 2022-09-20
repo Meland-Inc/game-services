1ad194f017fd5bba1709c097ffe7918cfd6a9af8
alextjj <254334386@qq.com>
2022-09-20 09:53:42 +0800

    main service received player kill monster event

--- src/global/web3Message/land_id.go
+++ src/global/web3Message/land_id.go
@@ -4,26 +4,26 @@ const landIdTemplates int64 = 10000000
 
 // land Id 和 xy 坐标系转换关系
 
-func XyToLandId(r, c int16) int64 {
+func XyToLandId(x, y int32) int64 {
 	// 例： 1000033300000004 RC 各占8位 总共16位
 	// 10000333 第一位 1 代表负数  333 为R坐标 中间用0填充(1*10000000+333)
 	// 00000004 第一位 0 代表正数  4   为C坐标 中间用0填充(0*10000000+333)
 	var rTag, cTag int64
-	if r < 0 {
+	if x < 0 {
 		rTag = 1
-		r = -r
+		x = -x
 	}
-	if c < 0 {
+	if y < 0 {
 		cTag = 1
-		c = -c
+		y = -y
 	}
 
-	rOffset := rTag*landIdTemplates + int64(r)
-	cOffset := cTag*landIdTemplates + int64(c)
+	rOffset := rTag*landIdTemplates + int64(x)
+	cOffset := cTag*landIdTemplates + int64(y)
 	return rOffset*landIdTemplates*10 + cOffset
 }
 
-func LandIdToXy(landId int64) (r, c int16) {
+func LandIdToXy(landId int64) (x, y int32) {
 	// 例： 1000033300000004 RC 各占8位 总共16位
 	// 10000333 第一位 1 代表负数  333 为R坐标 中间用0填充(1*10000000+333)
 	// 00000004 第一位 0 代表正数  4   为C坐标 中间用0填充(0*10000000+333)
@@ -32,15 +32,15 @@ func LandIdToXy(landId int64) (r, c int16) {
 	cOffset := landId % (landIdTemplates * 10)
 
 	rTag := rOffset / landIdTemplates
-	r = int16(rOffset % landIdTemplates)
+	x = int32(rOffset % landIdTemplates)
 	if rTag > 0 {
-		r = -r
+		x = -x
 	}
 
 	cTag := cOffset / landIdTemplates
-	c = int16(cOffset % landIdTemplates)
+	y = int32(cOffset % landIdTemplates)
 	if cTag > 0 {
-		c = -c
+		y = -y
 	}
 	return
 }
