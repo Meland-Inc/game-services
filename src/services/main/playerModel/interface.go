18414503778d3a9b331d0a144c6f061512b6c8dd
alextjj <254334386@qq.com>
2022-09-20 10:17:01 +0800

    main service received player death event

--- src/services/main/playerModel/interface.go
+++ src/services/main/playerModel/interface.go
@@ -2,8 +2,10 @@ package playerModel
 
 import (
 	"fmt"
+	"game-message-core/proto"
 
 	"github.com/Meland-Inc/game-services/src/global/component"
+	"github.com/Meland-Inc/game-services/src/global/configData"
 )
 
 func GetPlayerDataModel() (*PlayerDataModel, error) {
@@ -13,4 +15,35 @@ func GetPlayerDataModel() (*PlayerDataModel, error) {
 	}
 	dataModel, _ := iPlayerModel.(*PlayerDataModel)
 	return dataModel, nil
+}
+
+func (p *PlayerDataModel) OnPlayerDeath(
+	userId int64,
+	pos *proto.Vector3,
+	killerId int64, killType proto.EntityType, KillerName string,
+) error {
+	sceneData, err := p.GetPlayerSceneData(userId)
+	if err != nil {
+		return err
+	}
+
+	lvSetting := configData.ConfigMgr().RoleLevelCnf(sceneData.Level)
+	if lvSetting == nil {
+		return fmt.Errorf("role lv[%d] config not found", sceneData.Level)
+	}
+
+	// drop player current exp
+	deathLossExp := lvSetting.DeathExpLoss
+	if sceneData.Exp > deathLossExp {
+		p.DeductExp(userId, deathLossExp)
+		return nil
+	}
+
+	//  player max level item socket  lv -1
+	maxLvSocket, err := p.playerMaxLevelItemSlot(userId)
+	if err == nil && maxLvSocket.Level > 1 {
+		p.setPlayerItemSlotLevel(userId, maxLvSocket.Position, maxLvSocket.Level-1, true)
+	}
+
+	return err
 }
