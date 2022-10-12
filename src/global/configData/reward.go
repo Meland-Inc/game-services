7d6bac842173dd5acfd8a10da6116f8478fe5792
alextjj <254334386@qq.com>
2022-10-12 17:42:50 +0800

    fix task reward use reward.xlsx cnf data

--- src/global/configData/reward.go
+++ src/global/configData/reward.go
@@ -1,7 +1,11 @@
 package configData
 
 import (
+	"fmt"
+	"game-message-core/proto"
 	xlsxTable "game-message-core/xlsxTableData"
+
+	"github.com/Meland-Inc/game-services/src/common/matrix"
 )
 
 func (mgr *ConfigDataManager) initReward() error {
@@ -30,4 +34,53 @@ func (mgr *ConfigDataManager) RewardCnfById(id int32) *xlsxTable.RewardTableRow 
 		return nil
 	}
 	return &cnf
+}
+
+func RandomRewardItems(rewardId int32) (rewards []*proto.ItemBaseInfo, err error) {
+	if rewardId == 0 {
+		return rewards, nil
+	}
+
+	cnf := ConfigMgr().RewardCnfById(rewardId)
+	if cnf == nil {
+		return rewards, fmt.Errorf("rewardId [%d] not found", rewardId)
+	}
+
+	timeList, err := cnf.GetRewardTimeList()
+	if err != nil {
+		return rewards, err
+	}
+	rewardItemList, err := cnf.GetRewardList()
+	if err != nil {
+		return rewards, err
+	}
+
+	// 随机奖励次数
+	var rewardTimes int32
+	rnd := matrix.Random32(0, timeList.TotalWeight)
+	for _, ts := range timeList.RewardTimes {
+		if rnd <= ts.Weight {
+			rewardTimes = ts.Time
+			break
+		}
+		rnd -= ts.Weight
+	}
+
+	// 随机奖励物品
+	for i := 0; i < int(rewardTimes); i++ {
+		rnd := matrix.Random32(0, rewardItemList.TotalWeight)
+		for _, rs := range rewardItemList.Rewards {
+			if rnd <= rs.Weight {
+				rewards = append(rewards, &proto.ItemBaseInfo{
+					Cid:     rs.Cid,
+					Num:     rs.Num,
+					Quality: rs.Quantity,
+				})
+				break
+			}
+			rnd -= rs.Weight
+		}
+	}
+
+	return rewards, nil
 }
