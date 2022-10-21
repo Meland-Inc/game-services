4f0d0d4c45a47919da3f7f5adb8baf6450f5ccfb
alextjj <254334386@qq.com>
2022-10-21 19:54:06 +0800

    feat main service land and build client apis

---
+++ src/services/main/msgChannel/clientMsgHandle/land_handle.go
@@ -0,0 +1,40 @@
+package clientMsgHandle
+
+import (
+	"game-message-core/grpc/methodData"
+	"game-message-core/proto"
+
+	"github.com/Meland-Inc/game-services/src/services/main/playerModel"
+)
+
+func QueryLandsHandler(input *methodData.PullClientMessageInput, msg *proto.Envelope) {
+	agent := GetOrStoreUserAgent(input)
+	res := &proto.UpgradePlayerLevelResponse{}
+	respMsg := makeResponseMsg(msg)
+	defer func() {
+		if respMsg.ErrorMessage != "" {
+			respMsg.ErrorCode = 20006 // TODO: USE PROTO ERROR CODE
+		}
+		respMsg.Payload = &proto.Envelope_UpgradePlayerLevelResponse{UpgradePlayerLevelResponse: res}
+		ResponseClientMessage(agent, input, respMsg)
+	}()
+
+	if input.UserId < 1 {
+		respMsg.ErrorMessage = "upgrade player level Invalid User ID"
+		return
+	}
+
+	dataModel, err := playerModel.GetPlayerDataModel()
+	if err != nil {
+		respMsg.ErrorMessage = err.Error()
+		return
+	}
+
+	lv, exp, err := dataModel.UpgradePlayerLevel(input.UserId)
+	if err != nil {
+		respMsg.ErrorMessage = err.Error()
+		return
+	}
+	res.CurExp = int64(exp)
+	res.CurLevel = lv
+}
