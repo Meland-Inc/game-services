3b52a972d218e93ea5eadb99aaba14d8357de1d2
alextjj <254334386@qq.com>
2022-11-01 11:41:01 +0800

    fix main service start failed bug

--- scripts/dev/global.sh
+++ scripts/dev/global.sh
@@ -4,7 +4,7 @@ set -o errexit
 ## --------------- game config DB settings ----------------
 export GAME_CONFIG_DB_HOST=127.0.0.1
 export GAME_CONFIG_DB_USER=root
-export GAME_CONFIG_DB_PASS=123456
+export GAME_CONFIG_DB_PASS=root
 export GAME_CONFIG_DB_PORT=3306
 export GAME_CONFIG_DB_DATABASE=meland_cnf_dev
 
@@ -12,8 +12,8 @@ export GAME_CONFIG_DB_DATABASE=meland_cnf_dev
 export GAME_DB_HOST=127.0.0.1
 export GAME_DB_PORT=3306
 export GAME_DB_USER=root
-export GAME_DB_PASS=123456
-export GAME_DB_DATABASE=meland_game_data
+export GAME_DB_PASS=root
+export GAME_DB_DATABASE=meland_game_data_dev
 
 ## -------------- game TOKEN KEY ----------------
 export JWT_SECRET=xL6U7kZ3uLe.KggPUgXCN9!4223
