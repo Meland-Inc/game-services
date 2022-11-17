a9130b3c8e0cd9c58e2a12c54c3ad7948862dacb
alextjj <254334386@qq.com>
2022-11-17 21:51:46 +0800

    multi select services by service type

--- src/services/manager/dapr/calls/handle_select_service.go
+++ src/services/manager/dapr/calls/handle_select_service.go
@@ -3,7 +3,9 @@ package daprCalls
 import (
 	"context"
 	"fmt"
+	base_data "game-message-core/grpc/baseData"
 	"game-message-core/grpc/methodData"
+	"game-message-core/proto"
 
 	"github.com/Meland-Inc/game-services/src/common/daprInvoke"
 	"github.com/Meland-Inc/game-services/src/common/serviceLog"
@@ -39,5 +41,40 @@ func SelectServiceHandler(ctx context.Context, in *common.InvocationEvent) (*com
 		output.UpdateAt = serviceData.UpdateAt
 	}
 	serviceLog.Info("select service res = %+v", output)
+	return daprInvoke.MakeOutputContent(in, output)
+}
+
+func MultiSelectServiceHandler(ctx context.Context, in *common.InvocationEvent) (*common.Content, error) {
+	input := &methodData.MultiSelectServiceInput{}
+	err := grpcNetTool.UnmarshalGrpcData(in.Data, input)
+	if err != nil {
+		return nil, err
+	}
+
+	serviceLog.Info("received multi select service input: %+v", input)
+
+	allService := controller.GetInstance().AllServices()
+	output := &methodData.MultiSelectServiceOutput{}
+	for _, s := range allService {
+		if s.ServiceType != input.ServiceType {
+			continue
+		}
+		if input.ServiceType == proto.ServiceType_ServiceTypeAgent &&
+			input.MapId != s.MapId {
+			continue
+		}
+		ser := base_data.ServiceData{
+			AppId:       s.AppId,
+			ServiceType: s.ServiceType,
+			Host:        s.Host,
+			Port:        s.Port,
+			MapId:       s.MapId,
+			Online:      s.Online,
+			MaxOnline:   s.MaxOnline,
+			CreatedAt:   s.CreateAt,
+			UpdatedAt:   s.UpdateAt,
+		}
+		output.Services = append(output.Services, ser)
+	}
 	return daprInvoke.MakeOutputContent(in, output)
 }
