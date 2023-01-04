79411f472fe990b55a07b564f7cfcb3554711619
alextjj <254334386@qq.com>
2023-01-04 11:46:25 +0800

    main service mint and take NFT GRPC API

--- src/global/grpcAPI/grpcInvoke/gameServiceUserNFT.go
+++ src/global/grpcAPI/grpcInvoke/gameServiceUserNFT.go
@@ -4,19 +4,18 @@ import (
 	"encoding/json"
 	"errors"
 	"game-message-core/grpc"
+	base_data "game-message-core/grpc/baseData"
 	"game-message-core/grpc/methodData"
 
 	"github.com/Meland-Inc/game-services/src/common/daprInvoke"
 	"github.com/Meland-Inc/game-services/src/common/serviceLog"
-	"github.com/Meland-Inc/game-services/src/common/time_helper"
 	"github.com/Meland-Inc/game-services/src/global/grpcAPI/grpcNetTool"
 )
 
 func RPCMainServiceTakeNFT(userId int64, nfts []methodData.TakeNftData) error {
 	input := &methodData.MainServiceActionTakeNftInput{
-		MsgVersion: time_helper.NowUTCMill(),
-		UserId:     userId,
-		TakeNfts:   nfts,
+		UserId:   userId,
+		TakeNfts: nfts,
 	}
 	inputBytes, err := json.Marshal(input)
 	if err != nil {
@@ -36,6 +35,39 @@ func RPCMainServiceTakeNFT(userId int64, nfts []methodData.TakeNftData) error {
 	}
 
 	output := &methodData.MainServiceActionTakeNftOutput{}
+	err = grpcNetTool.UnmarshalGrpcData(outBytes, output)
+	if err != nil {
+		return err
+	}
+	if !output.Success {
+		return errors.New(output.FailedMsg)
+	}
+	return nil
+}
+
+func RPCMainServiceMintNFT(userId int64, item base_data.GrpcItemBaseInfo) error {
+	input := &methodData.MainServiceActionMintNftInput{
+		UserId: userId,
+		Item:   item,
+	}
+	inputBytes, err := json.Marshal(input)
+	if err != nil {
+		serviceLog.Error("Marshal MainServiceActionMintNftInput failed err: %+v", err)
+		return err
+	}
+
+	outBytes, err := daprInvoke.InvokeMethod(
+		string(grpc.GAME_SERVICE_APPID_MAIN),
+		string(grpc.MainServiceActionMintNFT),
+		inputBytes,
+	)
+	serviceLog.Info("call MainService MintNFT outBytes = %+v, err:%+v", string(outBytes), err)
+	if err != nil {
+		serviceLog.Error("call MainService MintNFT failed err:%+v", err)
+		return err
+	}
+
+	output := &methodData.MainServiceActionMintNftOutput{}
 	err = grpcNetTool.UnmarshalGrpcData(outBytes, output)
 	if err != nil {
 		return err
--- src/tool/main.go
+++ src/tool/main.go
@@ -22,21 +22,21 @@ func main() {
 
 func makeTaskTestItem(userId int64, posX, posY int32) {
 	for itemId := 3010101; itemId <= 3010102; itemId++ {
-		err := grpcInvoke.MintNFT(userId, int32(itemId), 10, 1, posX, posY)
+		err := grpcInvoke.Web3MintNFT(userId, int32(itemId), 10, 1, posX, posY)
 		if err != nil {
 			serviceLog.Error("mint nft[%d] failed: %v", itemId, err)
 		}
 	}
 
 	for itemId := 3010201; itemId <= 3010202; itemId++ {
-		err := grpcInvoke.MintNFT(userId, int32(itemId), 10, 1, posX, posY)
+		err := grpcInvoke.Web3MintNFT(userId, int32(itemId), 10, 1, posX, posY)
 		if err != nil {
 			serviceLog.Error("mint nft[%d] failed: %v", itemId, err)
 		}
 	}
 
 	for itemId := 1010001; itemId <= 1010012; itemId++ {
-		err := grpcInvoke.MintNFT(userId, int32(itemId), 1, 1, posX, posY)
+		err := grpcInvoke.Web3MintNFT(userId, int32(itemId), 1, 1, posX, posY)
 		if err != nil {
 			serviceLog.Error("mint nft[%d] failed: %v", itemId, err)
 		}
@@ -46,42 +46,42 @@ func makeTaskTestItem(userId int64, posX, posY int32) {
 
 func makeTestCraftItems(userId int64, posX, posY int32) {
 	for itemId := 3010101; itemId <= 3010102; itemId++ {
-		err := grpcInvoke.MintNFT(userId, int32(itemId), 10, 1, posX, posY)
+		err := grpcInvoke.Web3MintNFT(userId, int32(itemId), 10, 1, posX, posY)
 		if err != nil {
 			serviceLog.Error("mint nft[%d] failed: %v", itemId, err)
 		}
 	}
 
 	for itemId := 3010201; itemId <= 3010202; itemId++ {
-		err := grpcInvoke.MintNFT(userId, int32(itemId), 10, 1, posX, posY)
+		err := grpcInvoke.Web3MintNFT(userId, int32(itemId), 10, 1, posX, posY)
 		if err != nil {
 			serviceLog.Error("mint nft[%d] failed: %v", itemId, err)
 		}
 	}
 
 	for itemId := 60000001; itemId <= 60000020; itemId++ {
-		err := grpcInvoke.MintNFT(userId, int32(itemId), 10, 1, posX, posY)
+		err := grpcInvoke.Web3MintNFT(userId, int32(itemId), 10, 1, posX, posY)
 		if err != nil {
 			serviceLog.Error("mint nft[%d] failed: %v", itemId, err)
 		}
 	}
 
 	for itemId := 4010001; itemId <= 4010012; itemId++ {
-		err := grpcInvoke.MintNFT(userId, int32(itemId), 1, 1, posX, posY)
+		err := grpcInvoke.Web3MintNFT(userId, int32(itemId), 1, 1, posX, posY)
 		if err != nil {
 			serviceLog.Error("mint nft[%d] failed: %v", itemId, err)
 		}
 	}
 
 	for itemId := 4010901; itemId <= 4010902; itemId++ {
-		err := grpcInvoke.MintNFT(userId, int32(itemId), 1, 1, posX, posY)
+		err := grpcInvoke.Web3MintNFT(userId, int32(itemId), 1, 1, posX, posY)
 		if err != nil {
 			serviceLog.Error("mint nft[%d] failed: %v", itemId, err)
 		}
 	}
 
 	for itemId := 4020001; itemId <= 4020002; itemId++ {
-		err := grpcInvoke.MintNFT(userId, int32(itemId), 1, 1, posX, posY)
+		err := grpcInvoke.Web3MintNFT(userId, int32(itemId), 1, 1, posX, posY)
 		if err != nil {
 			serviceLog.Error("mint nft[%d] failed: %v", itemId, err)
 		}
